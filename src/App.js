f666b48c2176b7173a0182aafc2fc6efa3a59b83
wangxing <18912540921@163.com>
2018-07-27 13:16:16 +0800

    project can work

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React, { Component } from 'react'
+import React from 'react'
+import './App.css'
 import { Route } from 'react-router-dom'
 import ListBooks from './ListBooks'
 import SearchBooks from './SearchBooks'
 import * as BooksAPI from './BooksAPI'
 import * as _ from 'lodash'
-import './App.css'
 
-class BooksApp extends Component {
+
+class BooksApp extends React.Component {
   state = {
     /**
      * TODO: Instead of using this state variable to keep track of which page
@@ -14,10 +15,10 @@ class BooksApp extends Component {
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
      */
-    books: {}, // currently_reading 正在阅读, wantToRead 想要阅读, read 已读
-    searchList: [] // 搜索列表
+    books: {},
+    searchList: []
   }
-  // 应用启动时请求一次图书列表
+  //init
   componentDidMount() {
     BooksAPI.getAll().then((books) => {
       let obj = {}
@@ -31,19 +32,18 @@ class BooksApp extends Component {
       this.setState({ books: obj })
     })
   }
-  // 下拉选项切换书籍栏目
+
+  //drop-down menu
   changeSelect(shelf, key, item) {
     BooksAPI.update(item, shelf).then((book) => {
       this.setState((state) => {
         const { books, searchList} = this.state
         item.shelf = shelf;
-        // 不为书籍 shelf 不为 none 时代表添加到列表
+
         if(shelf !== 'none'){
           books[shelf] = books[shelf].concat([ item ])
         }
-        // 判断 key 的 typeof,
-        // 如果值为 number 从搜索列表删除当前已切换阅读栏目的书籍，
-        // 如果值为 string 书籍列表切换
+
         if(typeof key === 'number'){
           searchList.splice(key,1)
           this.setState((state) => ({
@@ -59,7 +59,7 @@ class BooksApp extends Component {
     })
   }
 
-  // 搜索书籍
+  // search
   searchBooks = _.debounce((query) => {
     if(query){
       BooksAPI.search(query).then((list) => {
@@ -68,18 +68,17 @@ class BooksApp extends Component {
             let books = Object.values(state.books)
             let booksList = []
             for(let i = 0; i < books.length; i++){
-              // 解构
+
               booksList = [...booksList, ...books[i]]
             }
 
             books = booksList
 
             const newSearchList = list.map((book) => {
-              // 如果该图书在书架中，会返回该图书，否则返回 undefined
               const searchListInshelfBook = books.find(
                 shelfBook => shelfBook.id === book.id
               )
-              // 同步 shelf 值，并返回该新的图书对象
+
               return {
                 ...book,
                 shelf: searchListInshelfBook ? searchListInshelfBook.shelf : 'none'
---
+++ src/ListBooks.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const ListBooks = ({ books, onChangeSelect }) => {
+	// 监听select
+	const handleChange = (value, key, item) => {
+		if(onChangeSelect){
+			onChangeSelect(value, key, item)
+		}
+	}
+
+	return (
+	  <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+          <div>
+       	  	{Object.keys(books).map((key) => (
+           	  	<div className="bookshelf" key={key}>
+                  <h2 className="bookshelf-title">{key === 'currentlyReading' ? 'Currently Reading' : key === 'wantToRead' ? 'Want to Read' : 'Read'}</h2>
+                  <div className="bookshelf-books">
+                    <ol className="books-grid">
+                      {books[key].map((book, index) => (
+	                      <li key={book.id}>
+	                        <div className="book">
+	                          <div className="book-top">
+                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail })` }}></div>
+	                            <div className="book-shelf-changer">
+	                              <select value={book.shelf} onChange={(event) => handleChange(event.target.value, key, book)}>
+	                                <option value="disabled" disabled>Move to...</option>
+	                                <option value="currentlyReading">Currently Reading</option>
+	                                <option value="wantToRead">Want to Read</option>
+	                                <option value="read">Read</option>
+	                                <option value="none">None</option>
+	                              </select>
+	                            </div>
+	                          </div>
+		                      <div className="book-title">{book.title || '暂无书名'}</div>
+		                      {!book.authors && (
+		                      	<div className="book-authors">佚名</div>
+		                  	  )}
+	                          {book.authors.map((authors) => (
+	                          	<div className="book-authors" key={authors}>{authors}</div>
+                          	  ))}
+	                        </div>
+	                      </li>
+      		       	  ))}
+                    </ol>
+                  </div>
+                </div>
+   	  		))}
+          </div>
+        </div>
+        <div className="open-search">
+          <Link to="/search" >Add a book</Link>
+        </div>
+      </div>
+	)
+}
+
+export default ListBooks
